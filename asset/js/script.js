3adcf138c60ccb89f6426ef4752114574fb096e0
OnlineProjectsGit <emtech.projects.live@gmail.com>
2024-01-24 16:23:38 +1100

    Fixed unmargined parallax

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,10 +1,20 @@
 window.addEventListener("scroll", function(){
     const parallax = document.querySelector('.parallax')
+    const aboutUs = document.querySelector('.about-us')
     let x = window.scrollY
     console.log(x)
-    if(x > 370) {
+
+    // HIDE PARALLAX
+    if(x > 420) {
         parallax.classList.add("active")
     } else {
         parallax.classList.remove("active")
+    }
+
+    // SHOW ABOUT US
+    if(x > 600) {
+        aboutUs.style.visibility = "visible"
+    } else {
+        aboutUs.style.visibility = "hidden"
     }
 })
